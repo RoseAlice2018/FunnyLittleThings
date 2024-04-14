e719393eb47ed93377cb8a484ea76e6257f7c2c2
RoseAlice2018 <1505243015@qq.com>
2024-04-14 18:47:15 +0800

    [PythonLearning] add PythonLearning

---
+++ PythonLearning/python1.py
@@ -0,0 +1,16 @@
+
+message = "python "
+message = message.rstrip()
+print(message)
+
+# practice 1
+message = "Eric slow"
+print("Hello {} , you are stupid".format(message))
+
+# practice 2
+print("Hello {}".format(message.upper()))
+print("Hello {}".format(message.lower()))
+print("Hello {}".format(message.title()))
+
+# practice 3
+print('Hello I said "stupid"')
