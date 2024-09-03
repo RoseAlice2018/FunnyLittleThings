d6a22f2107000d191fb3f9aa4421de05df7efef6
RoseAlice2018 <1505243015@qq.com>
2024-09-03 17:46:02 +0800

    [WordsCal] 优化Readme

--- PythonLearning/quant/readandsum.py
+++ PythonLearning/quant/readandsum.py
@@ -12,11 +12,67 @@
 
 import numpy as np
 import abc
-
+from typing import Callable
 
 def assert_msg(condition, msg):
     if not condition:
         raise Exception(msg)
+
+
+class Strategy(metaclass=abc.ABCMeta):
+    """
+    抽象策略类，用于定义交易策略
+
+    如果要定义自己的策略类，需要继承这个基类， 并实现两个抽象方法：
+    Strategy.init
+    Strategy.next
+    """
+    def __init__(self, broker, data):
+        """
+        构造策略对象
+
+        @params broker: ExchangeAPI 交易API接口，用于模拟交易
+        @params data:   list 行情交易数据
+        """
+        self._indicators = []
+        self.broker = broker # type: _Broker
+        self._data  = data   # type: _Data
+        self._tick  = 0
+        pass
+
+    def I(self, func: Callable, * args) -> np.ndarray:
+        """
+        计算买卖指标向量。 买卖指标向量是一个数组， 长度和历史数据对应
+        用于判定这个时间点上需要进行“买”还是“卖”
+        """
+        value = func(*args)
+        value = np.asarray(value)
+        assert_msg(value.shape[-1] == len(self._data.Close), '指示器长度必须和data长度相同')
+        self._indicators.append(value)
+        return value
+
+    @property
+    def tick(self):
+        return self._tick
+    
+    @abc.abstractmethod
+    def init(self):
+        """
+        初始化策略。在策略回测/执行过程中调用一次，用于初始化策略内部的状态
+        """
+        pass
+
+    @abc.abstractmethod
+    def next(self, tick):
+        """
+        步进函数，执行第tick步的策略。tick代表当前的时间
+        """
+        pass
+
+    def buy(self):
+        self._broker.buy()
+    
+    
     
 
 class Backtest:
@@ -25,6 +81,7 @@ class Backtest:
 
     初始化的时候调用Backtest.run来时回测
     """
+
 
     def __init__(self,
                  data: pd.DataFrame,
